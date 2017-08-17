c7331c4d27acddffcaef2134e7fc5bf8780af3db
jaydon1993 <jaydon1993@gmail.com>
2017-08-18 09:41:09 +1200

    v2 Unit Test Complete

---
+++ SDV502A1UnitTest/SDV502A1UnitTest/InputParcelDestinationZoneAndWeight_Should.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FastwayCourier;
+
+namespace SDV502A1UnitTest
+{
+    [TestClass]
+    public class InputParcelZoneColourAndWeight_Should
+    {
+        // Summary:
+        // CalculateQuote() is used to get a price and the amount of excess tickets needed for a parcel of certain weight 
+        // being delivered within a specified colour zone.
+
+        // Parameters:
+        // weight: Specifies the weight of the parcel to be delivered in kilograms.ArgumentOutOfRangeException thrown if the weight 
+        // is 0 or less. ArgumentOutOfRangeException thrown if the weight is greater than 25.
+        // zone: Specifies the coloured zone where the parcel is to be delivered.KeyNotFoundException thrown if the delivery zone 
+        // doesn't exist or is invalid.
+
+        // Returns:
+        // An object containing the total quoted price, and number of excess tickets to be used.
+
+        ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+
+        Decimal Weight = 24.999m;
+
+        [TestMethod]
+        public void ReturnExcessTicketsAndPrice_WhenZoneIsBlueAndWeightIsX()
+        {
+            // Arrange
+            //ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            ParcelQuoteResult ActualParcelQuote = pqfn.CalculateQuote(Weight, "Blue");
+            // Assert
+            Assert.AreEqual(0, ActualParcelQuote.ExcessTickets);
+            Assert.AreEqual(6.95m, ActualParcelQuote.Price);
+
+        }
+
+        [TestMethod]
+        public void ReturnExcessTicketsAndPrice_WhenZoneIsPinkAndWeightIsX()
+        {
+            // Arrange
+            //ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            ParcelQuoteResult ActualParcelQuote = pqfn.CalculateQuote(Weight, "Pink");
+            // Assert
+            Assert.AreEqual(0, ActualParcelQuote.ExcessTickets);
+            Assert.AreEqual(4.15m, ActualParcelQuote.Price);
+
+        }
+        
+        [TestMethod]
+        public void ReturnExcessTicketsAndPrice_WhenZoneIsOrangeAndWeightIsX()
+        {
+            // Arrange
+            //ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            ParcelQuoteResult ActualParcelQuote = pqfn.CalculateQuote(Weight, "Orange");
+            // Assert
+            Assert.AreEqual(2, ActualParcelQuote.ExcessTickets);
+            Assert.AreEqual(25.35m, ActualParcelQuote.Price);
+
+        }
+
+        [TestMethod]
+        public void ReturnExcessTicketsAndPrice_WhenZoneIsLimeAndWeightIsX()
+        {
+            // Arrange
+            //ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            ParcelQuoteResult ActualParcelQuote = pqfn.CalculateQuote(Weight, "Lime");
+            // Assert
+            Assert.AreEqual(1, ActualParcelQuote.ExcessTickets);
+            Assert.AreEqual(14.90m, ActualParcelQuote.Price);
+
+        }
+    }
+}
---
+++ SDV502A1UnitTest/SDV502A1UnitTest/InputShippingDestination_Should.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FastwayCourier;
+
+namespace SDV502A1UnitTest
+{
+    [TestClass]
+    public class InputShippingDestination_Should
+    {
+
+        //Summary:
+        //GetDestinationZone() is used to lookup the the coloured zone for pricing of a specified delivery destination - town, 
+        // city, etc.
+
+        // Parameters:
+        // destination: Specifies the location where a parcel may be delivered - town, city, etc.KeyNotFoundException 
+        // is thrown if the destination specified does not exist or is invalid.
+
+        // Returns:
+        // A string value containing the coloured zone where the specified destination location exists.
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsTakaka()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Takaka");
+            // Assert
+            Assert.AreEqual("Blue", ActualZone);
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsHavelock()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Havelock");
+            // Assert
+            Assert.AreEqual("Blue", ActualZone);
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsRiwaka()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Riwaka");
+            // Assert
+            Assert.AreEqual("Blue", ActualZone);
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsSeddon()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Seddon");
+            // Assert
+            Assert.AreEqual("Blue", ActualZone);
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsWard()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Ward");
+            // Assert
+            Assert.AreEqual("Blue", ActualZone);
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsWaihopaiValley()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Waihopai Valley");
+            // Assert
+            Assert.AreEqual("Blue", ActualZone);
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsMotueka()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Motueka");
+            // Assert
+            Assert.AreEqual("Pink", ActualZone);
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsAtawhai()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Atawhai");
+            // Assert
+            Assert.AreEqual("Pink", ActualZone);
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsMapua()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Mapua");
+            // Assert
+            Assert.AreEqual("Pink", ActualZone);
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsNelson()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Nelson");
+            // Assert
+            Assert.AreEqual("Pink", ActualZone);
+
+
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsMaitai()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Maitai");
+            // Assert
+            Assert.AreEqual("Pink", ActualZone);
+
+
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsHope()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Hope");
+            // Assert
+            Assert.AreEqual("Pink", ActualZone);
+
+
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsBrightwater()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Brightwater");
+            // Assert
+            Assert.AreEqual("Pink", ActualZone);
+
+
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsWakefield()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Wakefield");
+            // Assert
+            Assert.AreEqual("Pink", ActualZone);
+
+
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsPicton()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Picton");
+            // Assert
+            Assert.AreEqual("Pink", ActualZone);
+
+
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsRenwick()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Renwick");
+            // Assert
+            Assert.AreEqual("Pink", ActualZone);
+
+
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsBlenheim()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Blenheim");
+            // Assert
+            Assert.AreEqual("Pink", ActualZone);
+
+
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsMurchison()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Murchison");
+            // Assert
+            Assert.AreEqual("Lime", ActualZone);
+
+
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsNelsonLakes()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Nelson Lakes");
+            // Assert
+            Assert.AreEqual("Lime", ActualZone);
+
+
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsKaikoura()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Kaikoura");
+            // Assert
+            Assert.AreEqual("Orange", ActualZone);
+
+
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsReefton()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Reefton");
+            // Assert
+            Assert.AreEqual("Orange", ActualZone);
+
+
+        }
+
+        [TestMethod]
+        public void ReturnDestinationZoneColour_WhenDestinationIsHanmerSprings()
+        {
+            // Arrange
+            ParcelQuoteFromNelson pqfn = new ParcelQuoteFromNelson();
+            // Act
+            string ActualZone = pqfn.GetDestinationZone("Hanmer Springs");
+            // Assert
+            Assert.AreEqual("Orange", ActualZone);
+
+
+        }
+    }
+}
